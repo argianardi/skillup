a6607a3570be7814f8880e1d5a193282c97f07e2
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-14 10:44:39 +0700

    initialize

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    // Mass Assignment
+    // Cara 1
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon'
+    ];
+
+    // Cara 2
+    // protected $guarded = [
+    //     'id'
+    // ];
+}
---
+++ app/Models/Course.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'about',
+        'path_trailer',
+        'thumbnail',
+        'teacher_id',
+        'category_id',
+    ];
+}
