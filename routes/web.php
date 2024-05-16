eaa25ac85fb2130aee1ceaa5741997cb6439ddcb
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-16 09:09:02 +0700

    Feat: Add admin and teacher routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseVideoController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscribeTransactionController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/details/{course:slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/pricing', [FrontController::class])->name('front.pricing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +21,32 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout')->middleware('role:student');
+    Route::post('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout.store')->middleware('role:student');
+
+    Route::get('/learning/{course}/{courseVideoId}', [FrontController::class, 'learning'])->name('front.learning')->middleware('role:student|teacher|owner');
+
+    // Admin Route
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('categories', CategoryController::class)
+            ->middleware('role:owner');
+
+        Route::resource('teachers', TeacherController::class)
+            ->middleware('role:owner');
+
+        Route::resource('courses', CourseController::class)
+            ->middleware('role:owner|teacher');
+
+        Route::resource('subscribe_transactions', SubscribeTransactionController::class)
+            ->middleware('role:owner');
+
+        Route::get('/add/video/{course:id}', [CourseVideoController::class, 'create'])->name('course.add_video')->middleware('role:teacher|owner');
+        Route::post('/add/video/{course:id}', [CourseVideoController::class, 'store'])->name('course.add_video.save')->middleware('role:teacher|owner');
+
+        Route::resource('course_videos', CourseVideoController::class)
+            ->middleware('role:owner|teacher');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
