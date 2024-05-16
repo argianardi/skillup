091ab290c5e0f0433b1edc9a34c61f54adb40520
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-16 09:16:28 +0700

    Feat: Add default owner data

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ownerRole = Role::create([
+            'name' => 'owner'
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student'
+        ]);
+
+        $teacherRole = Role::create([
+            'name' => 'teacher'
+        ]);
+
+        $userOwner = User::create([
+            'name' => 'owner',
+            'occupation' => 'wiraswasta',
+            'avatar' => 'images/default-avatar.png',
+            'email' => 'owner@owner.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $userOwner->assignRole($ownerRole);
+    }
+}
