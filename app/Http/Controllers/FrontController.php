7a2859230943a3e585e154ff47fdbc596118b023
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-21 12:34:25 +0700

    Feat: Add landing page controller

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,18 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSubscribeTransactionRequest;
+use App\Models\Category;
 use App\Models\Course;
+use App\Models\SubscribeTransaction;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
     public function index()
     {
-        return view('front.index');
+        $categories = Category::orderByDesc('id')->get();
+        $courses = Course::with(['category', 'teacher', 'students'])->orderByDesc('id')->get();
+
+        return view('front.index', compact('courses', 'categories'));
     }
 
     public function details(Course $course)
     {
-        // dd($course);
-        return view('front.details');
+        return view('front.course-details', compact('course'));
+    }
+
+    public function learning(Course $course, $courseVideoId)
+    {
+        $user = Auth::user();
+
+        if (!$user->hasActiveSubscription()) {
+            return redirect()->route('front.pricing');
+        }
+
+        $video = $course->course_videos->firstWhere('id', $courseVideoId);
+        $user->courses()->syncWithoutDetaching($course->id);
+
+        return view('front.learning', compact('course', 'video'));
+    }
+
+    public function  pricing()
+    {
+        return view('front.pricing');
+    }
+
+    public function checkout()
+    {
+        if (Auth::user()->hasActiveSubscription()) {
+            return redirect()->route('front.index');
+        }
+
+        return view('front.checkout');
+    }
+
+    public function checkout_store(StoreSubscribeTransactionRequest $request)
+    {
+        $user = Auth::user();
+
+        if (Auth::user()->hasActiveSubscription()) {
+            return redirect()->route('front.index');
+        }
+
+        DB::transaction(function () use ($request, $user) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('proof')) {
+                $proofPath = $request->file('proof')->store('proofs', 'public');
+                $validated['proof'] = $proofPath;
+            }
+
+            $validated['user_id'] = $user->id;
+            $validated['total_amount'] = 500000;
+            $validated['is_paid'] = false;
+
+            SubscribeTransaction::create($validated);
+        });
+
+        return redirect()->route('dashboard');
+    }
+
+    public function category(Category $category)
+    {
+        $courses = $category->courses()->get();
+        return view('front.category-details', compact('courses'));
+    }
+
+    public function category_details(Course $course)
+    {
+        return view('front.category-details', compact('course'));
     }
 }
